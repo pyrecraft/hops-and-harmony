2c236223f179f86f9224bdb1115492ecbbd730e1
Jon Topielski <jontopielski@Jons-MacBook-Pro.local>
2019-11-24 09:15:50 -0800

    Update dialogue box system

--- src/things/DialogueBox.gd
+++ src/things/DialogueBox.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 signal text_complete
 
-export var font_height = 25
-export var font_width = 25
+export var font_height = 26
+export var font_width = 16
 export var full_text = ''
 
 var current_text = ''
@@ -13,15 +13,15 @@ var top_down_offset = 0 # translate into box margin left/right changes
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$RichTextLabel.bbcode_text = ''
-	set_text('Do you find it hard to do interesting things?')
+	set_text('What are you talking about?')
 
 func update_dialogue_box(text, font_height, font_width):
 	var box_dimens = get_dialogue_box_dimensions(text, font_height, font_width)
 	left_right_offset = box_dimens.x / 2.0
 	top_down_offset = box_dimens.y / 2.0
 	
-	$RichTextLabel.margin_top = -top_down_offset
-	$RichTextLabel.margin_bottom = top_down_offset
+	$RichTextLabel.margin_top = -top_down_offset * 2.0
+	$RichTextLabel.margin_bottom = 0
 	$RichTextLabel.margin_left = -left_right_offset
 	$RichTextLabel.margin_right = left_right_offset
 
@@ -34,8 +34,8 @@ func set_text(text):
 
 func _draw():
 	pass
-#	draw_rect(Rect2(Vector2(-left_right_offset, -top_down_offset), \
-#		Vector2(left_right_offset * 2.0, top_down_offset * 2.0)), Color.blue)
+	draw_rect(Rect2(Vector2(-left_right_offset, -top_down_offset * 2.0), \
+		Vector2(left_right_offset * 2.0, top_down_offset * 2.0)), Color.blue)
 
 func get_dialogue_box_dimensions(text, f_height, f_width):
 	if text == null or text.length() == 0:
@@ -45,41 +45,48 @@ func get_dialogue_box_dimensions(text, f_height, f_width):
 	var max_num = get_max_num(nums_array)
 	var num_sum = get_num_sum(nums_array)
 	
-	return get_most_squared_dimensions(nums_array, num_sum, max_num, f_height, f_width)
+	return get_most_squared_dimensions(split_text_array, nums_array, num_sum, max_num, f_height, f_width)
 
-func get_most_squared_dimensions(nums_array, num_sum, max_num, f_height, f_width):
-	var half_split_dimensions = get_squared_vector(1.0, nums_array, num_sum, max_num, f_height, f_width)
-	var half_split_squaredness = abs(1.0 - (half_split_dimensions.y / half_split_dimensions.x))
-	var more_than_half_split_dimensions = get_squared_vector(5.0, nums_array, num_sum, max_num, f_height, f_width)
+func get_most_squared_dimensions(text_array, nums_array, num_sum, max_num, f_height, f_width):
+#	var half_split_dimensions = get_squared_vector(5.0, nums_array, num_sum, max_num, f_height, f_width)
+#	var half_split_squaredness = abs(1.0 - (half_split_dimensions.y / half_split_dimensions.x))
+	var more_than_half_split_dimensions = get_squared_vector(10.0, text_array, nums_array, num_sum, max_num, f_height, f_width)
 	var more_than_half_split_squaredness = abs(1.0 - (more_than_half_split_dimensions.y / more_than_half_split_dimensions.x))
-	var even_more_than_half_split_dimensions = get_squared_vector(10.0, nums_array, num_sum, max_num, f_height, f_width)
-	var even_more_than_half_split_squaredness = abs(1.0 - (even_more_than_half_split_dimensions.y / even_more_than_half_split_dimensions.x))
-	if half_split_squaredness < more_than_half_split_squaredness and half_split_squaredness < even_more_than_half_split_squaredness:
-		return half_split_dimensions
-	elif more_than_half_split_squaredness < half_split_squaredness and \
-		more_than_half_split_squaredness < even_more_than_half_split_squaredness:
-		return more_than_half_split_dimensions
-	else:
-		return even_more_than_half_split_dimensions
+#	var even_more_than_half_split_dimensions = get_squared_vector(15.0, nums_array, num_sum, max_num, f_height, f_width)
+#	var even_more_than_half_split_squaredness = abs(1.0 - (even_more_than_half_split_dimensions.y / even_more_than_half_split_dimensions.x))
+	return more_than_half_split_dimensions
+#	if  half_split_squaredness < more_than_half_split_squaredness and half_split_squaredness < even_more_than_half_split_squaredness:
+#		return half_split_dimensions
+#	elif more_than_half_split_squaredness < half_split_squaredness and \
+#		more_than_half_split_squaredness < even_more_than_half_split_squaredness:
+#		return more_than_half_split_dimensions
+#	else:
+#		return even_more_than_half_split_dimensions
 
-func get_squared_vector(multiplier, nums, sum, max_num, f_height, f_width):
+func get_squared_vector(multiplier, words, nums, sum, max_num, f_height, f_width):
 	var width = max(sqrt(sum) * multiplier, max_num)
-	var line_count = 0
+	var line_count = 1
 	var current_line_width_remaining = width
 	var num_index = 0
 	var current_num = nums[num_index]
+	var max_line_width = 0
 	while num_index < nums.size():
 		if current_line_width_remaining - current_num >= 0:
+			print(words[num_index])
 			current_line_width_remaining -= current_num
 			num_index += 1
+			if width - current_line_width_remaining > max_line_width:
+				max_line_width = width - current_line_width_remaining
 			if num_index == nums.size():
 				break
 			current_num = nums[num_index]
 		elif current_line_width_remaining - current_num < 0:
+			if width - current_line_width_remaining > max_line_width:
+				max_line_width = width - current_line_width_remaining
 			current_line_width_remaining = width
 			line_count += 1
 	var height = line_count * f_height
-	return Vector2(width, height)
+	return Vector2(max_line_width, height)
 
 func convert_string_list_to_int(str_array):
 	var nums = []
